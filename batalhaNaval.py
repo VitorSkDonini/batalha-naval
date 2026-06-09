5f2a4734352b0721112eec7fc36f801750e26c78
VitorSkDonini <vitorskdonini3@gmail.com>
2026-06-09 18:45:14 -0300

    trocar 0 por barco e onda+printar matriz

--- batalhaNaval.py
+++ batalhaNaval.py
@@ -8,13 +8,23 @@ def criarTab():
             linha.append(0)
         tabReal.append(linha)
     return tabReal
+
+def tabuleiroVisivel(tabuleiroReal):
+    for i in tabuleiroReal:
+        for onda in i:
+            if onda == 0:
+                print('🌊', end= ' ')
+            elif onda == 1:
+                print('🚢', end= ' ')
+        print()
     
+
+def colocarBarcosJogador():
+    for barcos in range(5):
+        tabuleiroReal.insert(1)
+
 tabuleiroReal = criarTab()
 tabuleiroJogador = criarTab()
 tabuleiroMaquina = criarTab()
 
-# for i in range(10):
-#     for j in range(10):
-#         print(tabuleiroReal[i][j], end='  ')
-#     print()
-
+tabuleiroVisivel(tabuleiroReal)
