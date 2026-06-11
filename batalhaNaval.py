bfb8dc6f31e7ad925da19a4dc68182ff9134076d
VitorSkDonini <vitorskdonini3@gmail.com>
2026-06-10 22:54:35 -0300

    jogo funcionando/finalizado

--- batalhaNaval.py
+++ batalhaNaval.py
@@ -1,5 +1,5 @@
 import random
-
+# cria tabuleiro e troca para ser visivel
 def criarTab():
     tabReal= []
     for i in range(10):
@@ -46,7 +46,7 @@ def colocarBarcosRobo():
             contadorBarcos += 1
 
 
-# os atque tem q ser pro tab oposto
+# chama os ataques da maquina + jogador (os atque tem q ser pro tab oposto/ acertos retornam valor salvo)
 
 def ataqueMaquina():
     while True:
@@ -55,9 +55,11 @@ def ataqueMaquina():
         if tabuleiroJogadorVisivel[X][Y] == 0:
             if tabuleiroJogadorReal[X][Y]== 1:
                 tabuleiroJogadorVisivel[X][Y] = 'X'
+                print('A maquina acertou um barco seu')
                 return 1
             else:
                 tabuleiroJogadorVisivel[X][Y] = 'O'
+                print('A maquina errou')
                 return 0
 
 def ataqueJogador():
@@ -80,8 +82,9 @@ def ataqueJogador():
             print('coordenada invalida')
     
 
-#chama criacao de tab + barcos + ataques/ chama o que e mostrado mostrarTabuleiro(tabuleiroJogadorVisivel), mostrarTabuleiro(tabuleiroMaquinaVisivel) (sem barcos)
+#chama o jogo(menu e logica do jogo + pontos)
 def main():
+
     print('-----------------------------------------------------------------------------------------------------')
     print('\nBem vindo ao BATALHA NAVAL\n')
     print('Jogar| 1')
@@ -90,45 +93,52 @@ def main():
 
     escolha= int(input('escolha uma opcao:\n'))
 
-    contadorAcertosJogador = 5
-    contadorAcertosMaquina = 5
+    barcosJogador = 5
+    barcosMaquina = 5
+    if escolha == 1:
+        colocarBarcosJogador()
+        colocarBarcosRobo()
+        while barcosJogador > 0 and barcosMaquina > 0:
 
-    while True:
-        if escolha == 1:
-            colocarBarcosJogador()
-            colocarBarcosRobo()
-            while contadorAcertosJogador > 0 and contadorAcertosMaquina > 0:
+            print('-----------------------------------------------------------------------------------------------------')
+            print('Vez do jogador')
+            print('tabuleiro da maquina:')
+            tabuleiroVisivel(tabuleiroMaquinaVisivel)
+            print('escolha onde atacar:')
+            tiroJogador = ataqueJogador()
+            tabuleiroVisivel(tabuleiroMaquinaVisivel)
+            print('-----------------------------------------------------------------------------------------------------')
 
-                print('-----------------------------------------------------------------------------------------------------')
-                print(tabuleiroVisivel(tabuleiroMaquinaVisivel))
-                print('Vez do jogador')
-                print('escolha onde atacar:')
-                tiroJogador = ataqueJogador()
-                tabuleiroVisivel(tabuleiroMaquinaVisivel)
-                print('-----------------------------------------------------------------------------------------------------')
+            if tiroJogador == 1:
+                barcosMaquina -= 1
+                print('faltam',barcosMaquina, ' barcos da maquina')
+            if barcosMaquina == 0:
+                print('PARABENS!!\n Voce venceu')
+                break
 
-                if tiroJogador == 1:
-                    contadorAcertosMaquina -= 1
-                if contadorAcertosMaquina == 0:
-                    print('PARABENS!!\n Voce venceu')
-                    break
+            print('-----------------------------------------------------------------------------------------------------')
+            tiroMaquina = ataqueMaquina() 
+            if tiroMaquina == 1 :
+                barcosJogador -= 1
+                print('faltam',barcosJogador, 'barcos seus')
+            if barcosJogador == 0:
+                print('PERDEU!!\n A maquina venceu ')
+                break
+            print('Onde a maquina atacou')
+            tabuleiroVisivel(tabuleiroJogadorVisivel)
+            print('-----------------------------------------------------------------------------------------------------')
 
-                print('-----------------------------------------------------------------------------------------------------')
+    elif escolha == 2:
+         print('-----------------------------------------------------------------------------------------------------')
+         print('jogo fechado')
+         print('Obrigado por jogar')
 
-                tiroMaquina = ataqueMaquina() 
-                if tiroMaquina == 1 :
-                   contadorAcertosJogador -= 1
-                if contadorAcertosJogador == 0:
-                    print('PERDEU!!\n A maquina venceu ')
-                    break
-                tabuleiroVisivel(tabuleiroJogadorVisivel)
+    else:
+        print('opcao invalida')
 
-                print('-----------------------------------------------------------------------------------------------------')
-
+# gera matriz do tabuleiro+ carrega jogo
 tabuleiroJogadorVisivel = criarTab()
 tabuleiroJogadorReal = criarTab()
-
 tabuleiroMaquinaVisivel = criarTab()
 tabuleiroMaquinaReal = criarTab()
-
 main()
